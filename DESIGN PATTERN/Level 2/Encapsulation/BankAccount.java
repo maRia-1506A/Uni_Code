872b01f635109923db69b40143fb56b4841bb414
maRia-1506A <zannatulfarzana363@gmail.com>
2026-01-30 03:15:02 +0600

    encapsulation

---
+++ DESIGN PATTERN/Level 2/Encapsulation/BankAccount.java
@@ -0,0 +1,30 @@
+public class BankAccount {
+    private double balance;
+
+    public void deposite(double amount) {
+        balance+= amount;
+        System.out.println("Deposited amount: "+amount);
+
+    }
+
+    public void withdraw(double amount) {
+        balance-= amount;
+        if(balance < amount) {
+            System.out.println("Insufficent balance");
+        } else {
+            System.out.println("Withdrawn amount: "+amount);
+        }
+    }
+
+    public double checkBalance() {
+        return balance;
+        
+    }
+
+    public static void main(String[] args) {
+        BankAccount ba= new BankAccount();
+        ba.deposite(10000);
+        ba.withdraw(4000);
+        System.out.println("Final Balance: "+ba.checkBalance());
+    }
+}
---
+++ DESIGN PATTERN/Level 2/Encapsulation/Grade.java
@@ -0,0 +1,32 @@
+public class Grade {
+    private float m1, m2, m3;
+
+    public void inputMark(float m1, float m2, float m3) {
+        this.m1= m1;
+        this.m2= m2;
+        this.m3= m3;
+    }
+
+    public float average() {
+        return m1+m2+m3/3;
+    }
+
+    public void gradeDet(){
+        float avg= average();
+        if(avg >= 80) {
+            System.out.println("Grade: A+");
+        } else if(avg >75 && avg<80) {
+            System.out.println("Grade: A");
+        } else {
+            System.out.println("Grade: C");
+        }
+    }
+
+    public static void main(String[] args) {
+        Grade g= new Grade();
+        g.inputMark(96, 69, 85);
+        System.out.println("Average: "+g.average());
+        g.gradeDet();
+
+    }
+}
