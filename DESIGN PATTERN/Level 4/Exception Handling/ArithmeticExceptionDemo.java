f577a3e03753d84cdd59f69b2021d5614fba8ae4
maRia-1506A <zannatulfarzana363@gmail.com>
2026-02-02 23:47:40 +0600

    exception handling

---
+++ DESIGN PATTERN/Level 4/Exception Handling/ArithmeticExceptionDemo.java
@@ -0,0 +1,14 @@
+public class ArithmeticExceptionDemo {
+    public static void main(String[] args) {
+        try {
+            int result = 10 / 0;
+            System.out.println("Result: " + result);
+        }
+        catch (ArithmeticException e) {
+            System.out.println(e);
+        }
+
+        System.out.println("Continue....");
+    }
+
+}
