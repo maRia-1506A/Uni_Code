15ce300ebed251f2e37c214647450b41a6fa2bb8
maRia-1506A <zannatulfarzana363@gmail.com>
2026-02-08 01:29:01 +0600

    overloading and overriding

---
+++ DESIGN PATTERN/Level 3/Method Overloading & Overriding/Animal.java
@@ -0,0 +1,28 @@
+public class Animal {
+    void makeSound() {
+        System.out.println("Animal makes sound");
+    }
+    public static void main(String[] args) {
+        Animal a= new Animal();
+        Animal d= new Dog();
+        Animal c= new Cat();
+        
+        a.makeSound();
+        d.makeSound();
+        c.makeSound();
+    }
+}
+
+class Dog extends Animal {
+    @Override
+    void makeSound() {
+        System.out.println("Dog barks");
+    }
+}
+
+class Cat extends Animal {
+    @Override
+    void makeSound() {
+        System.out.println("Cat meows");
+    }
+}
---
+++ DESIGN PATTERN/Level 3/Method Overloading & Overriding/AreaCalculator.java
@@ -0,0 +1,17 @@
+public class AreaCalculator {
+    double calculateArea(double r) {
+        return 3.14 * r * r;
+
+    }
+
+    double calculateArea(double h, double w) {
+        return h*w;
+
+    }
+    public static void main(String[] args) {
+        AreaCalculator result= new AreaCalculator();
+        System.out.println("Area of a circle: "+result.calculateArea(1));
+        System.out.println("Area of a rectangle: "+result.calculateArea(2, 3));
+    }
+
+}
