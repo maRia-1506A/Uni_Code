ddb58c566276fb375958e5d56460f3b04dfb8fa4
maRia-1506A <zannatulfarzana363@gmail.com>
2026-02-21 23:38:54 +0600

    some modification in factory dm

--- DESIGN PATTERN/FACTORY DESIGN PATTERN/MarketplaceClient.java
+++ DESIGN PATTERN/FACTORY DESIGN PATTERN/MarketplaceClient.java
@@ -1,26 +1,26 @@
 // interface (WHAT payment can do)
 interface paymentInterface{
-    void pay (double amount);
+    String pay (double amount);
 }
 
 // CreditCardPayment class (HOW payment works)
 class CreditCardPayment implements paymentInterface {
-    public void pay(double amount) {
-        System.out.println("Paid by Card: "+amount);
+    public String pay(double amount) {
+        return "Paid by Card: "+amount;
     }
 }
 
 // PayPalPayment class (HOW payment works)
 class PayPalPayment implements paymentInterface {
-    public void pay(double amount) {
-        System.out.println("Paid by Paypal: "+amount);
+    public String pay(double amount) {
+       return "Paid by Paypal: "+amount;
     }
 }
 
 // BankTransferPayment class (HOW payment works)
 class BankTransferPayment implements paymentInterface{
-    public void pay(double amount) {
-        System.out.println("Paid by Bank Transfer: "+amount);
+    public String pay(double amount) {
+       return "Paid by Bank Transfer: "+amount;
     }
 }
 
@@ -40,3 +40,19 @@ class PaymentFactory {
     }
 }
 
+// client / main class 
+public class MarketplaceClient {
+    // Manage the Process & create object
+    // like what type of payment method will choose & whats the amount for transiction
+    public String pay(String type, double amount) {
+        paymentInterface pm= PaymentFactory.create(type);
+        return pm.pay(amount);
+    }
+
+    public static void main(String[] args) {
+        MarketplaceClient m= new MarketplaceClient();
+
+        System.out.println(m.pay("CreditCard", 10000));
+        System.out.println(m.pay("Paypal", 5000));
+    }
+}
